8decb1f4b19178112c2c11f364a373636cefd99f
shenling <shenling@cetcbigdata.com>
2019-06-19 18:09:29 +0800

    制作太阳系地球与月亮

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ThreeApp from './ThreeApp';
+import ThreePolar from  './Three-Polar';
+import RubiksCube from './RubiksCube';
 import './App.css';
 
 function App() {
   return (
-    <ThreeApp/>
+    <ThreePolar/>
   );
 }
 
--- src/ThreeApp.js
+++ src/ThreeApp.js
@@ -14,6 +14,8 @@ export default class ThreeApp extends React.Component {
   initThree = () => {
     let camera, scene, renderer, group, spotLight;
     let container = document.getElementById('scene');
+    let ball;
+    let v, a;
 
     initScene();
     // animate();
@@ -21,13 +23,13 @@ export default class ThreeApp extends React.Component {
     function initScene() {
       //创建场景
       scene = new THREE.Scene();
-      scene.background = new THREE.Color(0x696969);
       group = new THREE.Group();
       scene.add(group);
       
       //创建相机
       camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 2000);
-      camera.position.set(-10, 15, 500);
+      // camera.position.set(141, 80, 0);
+      camera.position.set(100, 30, 100)
       
       //添加鼠标控制效果
       let orbitControls = new Orbitcontrols(camera);
@@ -37,30 +39,46 @@ export default class ThreeApp extends React.Component {
       addSpotLight();
 
       //添加物体
-      // group.add(createCube(50,50,50, { x: 30, y: 30, z: 30 }));
-      let earth = createSphere(150, 15, 15, { x: 0, y: 0, z: 0 });
-      let ground = createPlane(200, 200, 10, 5, {x: 0, y: -200, z: 0})
-      // group.add(createPlane(20, 50, 1, 2,  { x: -30, y: 30, z: 30 }));
-      scene.add(ground);
-      group.add(earth)
+      let ground = createCube(110,1,110, { x: 0, y: -25, z: 0 });
+      let ground1 = createPlane(110, 100, 1, 1, {x: 0, y: -25, z: 0});
+      ball = createSphere(5, 50, 50, {x: 30, y: 15, z: 30});
+      scene.add(ball);
+      ground1.rotateX( -Math.PI * 0.5);
+      scene.add(ground1);
+      // group.add(earth)
 
+      v = 0;
+      a = -0.1;
 
       //创建渲染器
       renderer = new THREE.WebGLRenderer();
-      // renderer.setClearColor(0x000000, 1); // 设置默认背景色
 			renderer.setPixelRatio(window.devicePixelRatio); // 设置像素比，针对高清屏
       renderer.setSize( window.innerWidth, window.innerHeight);//渲染器大小尺寸
       container.appendChild( renderer.domElement );
 
       //动画效果
-      requestAnimationFrame(animate);
-      earth.rotation.x += 0.05;
-      renderer.render( scene, camera);
+      requestAnimationFrame(draw);
+    }
+
+    function draw() {
+      ball.position.y += v;
+      v += a;
+
+      if (ball.position.y <= -20) {
+        v = -v * 0.9;
+      }
+
+      if (Math.abs(v) < 0.0005) {
+        ball.position.y = -20;
+      }
+
+      renderer.render(scene, camera);
+      requestAnimationFrame(draw);
     }
 
     function createCube(x, y, z, position) {
       let geometry = new THREE.BoxGeometry(x, y, z); //create BoxGeometry object
-      let material = new THREE.MeshPhongMaterial({ color: 0xffffff}); //set the material of the cube
+      let material = new THREE.MeshStandardMaterial({ color: 0xffffff}); //set the material of the cube
       let cube = new THREE.Mesh( geometry, material); //Mesh contains the cube and the material, we can push the mesh object into our scenerio, and make it moves freely in the scene
       cube.receiveShadow = true;
       cube.position.set(position.x, position.y, position.z)
@@ -79,8 +97,8 @@ export default class ThreeApp extends React.Component {
     function createSphere(radius, widthSegment, heightSegment, position) {
       let sphereGeometry = new THREE.SphereGeometry(radius, widthSegment, heightSegment);
       let texture = new THREE.TextureLoader().load(require('./assets/Earth.png'));
-      let sphereMaterial = new THREE.MeshStandardMaterial({map: texture});
-      // let sphereMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+      // let sphereMaterial = new THREE.MeshStandardMaterial({map: texture});
+      let sphereMaterial = new THREE.MeshPhongMaterial({ color: 0xffff00, side: THREE.DoubleSide });
       let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
       sphere.castShadow = true;
       sphere.position.set(position.x, position.y, position.z);
@@ -88,19 +106,11 @@ export default class ThreeApp extends React.Component {
     }
     
     function addSpotLight() {
-      spotLight = new SpotLight(0xffffff);
-      spotLight.position.set(300,200,300);
+      spotLight = new THREE.SpotLight(0xffffff)
+      
+      spotLight.position.set(400,400,200);
       spotLight.castShadow = true;
-      // spotLight.shadow.mapSize.width = 2048;
-      // spotLight.shadow.mapSize.height = 2048;
       scene.add(spotLight)
-    }
-    
-    function animate () {
-      requestAnimationFrame(animate);
-      group.rotation.x += 0.005;
-      group.rotation.y += 0.005;
-      renderer.render( scene, camera);
     }
   }
 
